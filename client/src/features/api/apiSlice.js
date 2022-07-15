6dcddbdbcf35a6b78ffe621ff83446009e0d5818
Jamal-Deallie <66758645+Jamal-Deallie@users.noreply.github.com>
2022-07-15 17:25:17 -0500

    Added server to API Slice

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:4000/api/v1',
+    baseUrl: 'https://money-trees-app.herokuapp.com/api/v1',
 
     prepareHeaders: (headers, { getState }) => {
       const token = JSON.parse(localStorage.getItem('token'));
